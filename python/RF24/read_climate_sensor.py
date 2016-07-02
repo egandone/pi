7b554ed5e30bb9d44600cbc5e521acce708a4186
egan <edone@sympatico.ca>
2016-07-02 09:47:53 -0400

    added data upload to dweet.io

--- python/RF24/read_climate_sensor.py
+++ python/RF24/read_climate_sensor.py
@@ -4,18 +4,24 @@ from __future__ import print_function
 import time
 from RF24 import *
 import RPi.GPIO as GPIO
+import dweepy
 
 irq_gpio_pin = None
 
 # Setup for GPIO 22 CE and CE0 CSN with SPI Speed @ 8Mhz
 radio = RF24(RPI_V2_GPIO_P1_15, BCM2835_SPI_CS0, BCM2835_SPI_SPEED_8MHZ)
+thing_name = 'd5299e5a8688832804c2f51ecaf5192e'
 
 def try_read_data():
     if radio.available():
-        while radio.available():
-            len = radio.getDynamicPayloadSize()
-            receive_payload = radio.read(len)
-            print('Got payload size={} value="{}"'.format(len, receive_payload.decode('utf-8')))
+        len = radio.getDynamicPayloadSize()
+        receive_payload = radio.read(len)
+        print('Got payload size={} value="{}"'.format(len, receive_payload.decode('utf-8')))
+        values = receive_payload.split('|')
+        dweet = {'temp1': float(values[0].decode()), 'temp2': float(values[1].decode()), 'pressure': float(values[2].decode()), 'humidity': float(values[3].decode())}
+        print(dweet)
+        dweepy.dweet_for(thing_name, dweet)
+            
 
 pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2]
 #min_payload_size = 4
@@ -35,4 +41,5 @@ radio.startListening()
 # forever loop
 while 1:
     try_read_data()
+    time.sleep(15)
 
